b42baa9c36b95f0808d38ac8df68f6fd4e70e1e3
Douglas <douglas-negocios@hotmail.com>
2023-02-16 17:07:19 +0044

    Tentativa de resolver problema no Heroku

--- src/main/java/br/com/douglas/controller/instituicaobancaria/InstituicaoBancariaController.java
+++ src/main/java/br/com/douglas/controller/instituicaobancaria/InstituicaoBancariaController.java
@@ -2,7 +2,7 @@ package br.com.douglas.controller.instituicaobancaria;
 
 import br.com.douglas.controller.core.BaseController;
 import br.com.douglas.entity.entities.temp.InstituicaoBancaria;
-import br.com.douglas.mapper.BaseMapper;
+import br.com.douglas.mapper.instituicaobancaria.InstituicaoBancariaMapper;
 import br.com.douglas.mapper.instituicaobancaria.InstituicaoBancariaRequest;
 import br.com.douglas.mapper.instituicaobancaria.InstituicaoBancariaResponse;
 import br.com.douglas.service.interfaces.IBaseService;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/instituicao-bancaria")
 public class InstituicaoBancariaController extends BaseController<InstituicaoBancaria, InstituicaoBancariaRequest, InstituicaoBancariaResponse> {
-    protected InstituicaoBancariaController(IBaseService<InstituicaoBancaria> service, BaseMapper<InstituicaoBancaria, InstituicaoBancariaRequest, InstituicaoBancariaResponse> responseMapper) {
+    protected InstituicaoBancariaController(IBaseService<InstituicaoBancaria> service, InstituicaoBancariaMapper responseMapper) {
         super(service, responseMapper);
     }
 }
