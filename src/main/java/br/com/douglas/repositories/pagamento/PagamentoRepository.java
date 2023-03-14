d84c2df21815ebc2ae4432944a7258befba88826
Douglas <douglas-negocios@hotmail.com>
2023-03-14 17:44:29 +0044

    Adicionando somente descartes no dashboard de descartes

--- src/main/java/br/com/douglas/repositories/pagamento/PagamentoRepository.java
+++ src/main/java/br/com/douglas/repositories/pagamento/PagamentoRepository.java
@@ -14,6 +14,6 @@ public interface PagamentoRepository extends BaseRepository<Pagamento> {
     @Query(value = "select sum(p.valor) from Pagamento p where p.dataPagamento between :inicio and :fim")
     Optional<Integer> somaMensal(LocalDate inicio, LocalDate fim);
 
-    @Query(value = "select sum(p.valor) from Pagamento p where p.dataPagamento =:data ")
+    @Query(value = "select sum(p.valor) from Pagamento p where p.ctr != null and p.dataPagamento =:data ")
     Optional<Integer> somaSemanal(LocalDate data);
 }
