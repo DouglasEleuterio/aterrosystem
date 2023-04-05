1d3067dd8c8f2cc6dc8d4ecdaf65948e2ccea3ee
Douglas <douglas-negocios@hotmail.com>
2023-04-05 16:39:25 +0044

    Ordenação do veículo por placa

--- src/main/java/br/com/douglas/controller/veiculo/VeiculoController.java
+++ src/main/java/br/com/douglas/controller/veiculo/VeiculoController.java
@@ -1,13 +1,26 @@
 package br.com.douglas.controller.veiculo;
 
 import br.com.douglas.controller.core.BaseController;
+import br.com.douglas.controller.core.BaseControllerConstants;
 import br.com.douglas.mapper.BaseMapper;
+import br.com.douglas.mapper.transportador.TransportadorResponse;
 import br.com.douglas.mapper.veiculo.VeiculoRequest;
 import br.com.douglas.mapper.veiculo.VeiculoResponse;
 import br.com.douglas.entity.entities.temp.Veiculo;
+import br.com.douglas.rsql.jpa.util.SpecificationUtils;
 import br.com.douglas.service.interfaces.IBaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springdoc.core.converters.models.PageableAsQueryParam;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.SortDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/veiculo")
@@ -16,4 +29,17 @@ public class VeiculoController extends BaseController<Veiculo, VeiculoRequest, V
     protected VeiculoController(IBaseService<Veiculo> service, BaseMapper<Veiculo, VeiculoRequest, VeiculoResponse> responseMapper) {
         super(service, responseMapper);
     }
+
+    @GetMapping("/find-list")
+    @PageableAsQueryParam
+    @Operation(description = "Consulta os dados e filtrando utilizando o padrão **RSQL** sem paginação")
+    public List<VeiculoResponse> findList(@Parameter(description = BaseControllerConstants.FIND_PAGE_DOC)
+                                                @RequestParam(required = false) String search,
+                                                @SortDefault.SortDefaults({@SortDefault(sort = "placa", direction = Sort.Direction.ASC)}) Sort sort) {
+        if(Objects.nonNull(search) && !search.isBlank() && !search.isEmpty()) {
+            return service.findAll(SpecificationUtils.rsqlToSpecification(search), sort).stream().map(mapper::toResponse).toList();
+        }
+        return service.findAll(sort).stream().map(mapper::toResponse).toList();
+    }
+
 }
