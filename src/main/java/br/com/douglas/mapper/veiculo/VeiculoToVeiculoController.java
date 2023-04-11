6264d1af57681d326470648a7806dc77e463ce50
Douglas <douglas-negocios@hotmail.com>
2023-04-11 14:49:43 +0044

    Deixando apenas placa de ID

--- src/main/java/br/com/douglas/mapper/veiculo/VeiculoToVeiculoController.java
+++ src/main/java/br/com/douglas/mapper/veiculo/VeiculoToVeiculoController.java
@@ -1,6 +1,7 @@
 package br.com.douglas.mapper.veiculo;
 
 import br.com.douglas.mapper.transportador.TransportadorToVeiculoResponse;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 @Getter
@@ -12,6 +13,8 @@ public class VeiculoToVeiculoController {
 
     private String id;
     private String placa;
+    @JsonIgnore
     private Boolean ativo;
+    @JsonIgnore
     private TransportadorToVeiculoResponse transportador;
 }
